cf711cc0f06cc5c5c756a98f6532212e72a054d1
iamsundar18 <43379666+iamsundar18@users.noreply.github.com>
2021-06-05 20:36:13 +0530

    Add files via upload

---
+++ Day6_TourOfHero/TourOfHero/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Kilua' },
+    { id: 2, name: 'Gon' },
+    { id: 3, name: 'Kite' },
+    { id: 4, name: 'Asta' },
+    { id: 5, name: 'Levi' },
+    { id: 6, name: 'Yami' },
+    { id: 7, name: 'William' },
+    { id: 8, name: 'Meruem' },
+    { id: 9, name: 'Gin' },
+    { id: 10, name: 'Yuno' }
+];
