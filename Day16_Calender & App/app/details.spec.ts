33112158a83f4668318673f4844f7f95275ebafd
iamsundar18 <43379666+iamsundar18@users.noreply.github.com>
2021-06-18 20:58:49 +0530

    Delete Day16_Calender & App directory

--- Day16_Calender & App/app/details.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Details } from './details';
-
-describe('Details', () => {
-  it('should create an instance', () => {
-    expect(new Details()).toBeTruthy();
-  });
-});
--- Day16_Calender & App/app/details.ts
+++
@@ -1,9 +0,0 @@
-export class Details {
-
-    constructor(
-        public fname: any,
-        public lname: any,
-        public email: any,
-        public pass: any
-    ) { }
-}
